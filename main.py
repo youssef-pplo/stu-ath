16ee7396d3368329a1c23512a9f58d3a1c6c2ca7
Youssef pplo <Youssef@pplo.dev>
2025-08-05 00:53:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import requests
 
 app = FastAPI()
 
-origins = ["*"]  # Allow all origins for dev; restrict in prod
+origins = ["http://localhost:5173", "https://easybio2025.netlify.app"]  
 
 app.add_middleware(
     CORSMiddleware,
