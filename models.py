c598ad29262725c9c7be7cb729ade02d8290c1f1
Youssef pplo <Youssef@pplo.dev>
2025-08-05 01:16:52 +0300

    Update models.py

--- models.py
+++ models.py
@@ -6,10 +6,11 @@ class Student(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String)
-    phone = Column(String, unique=True, index=True)
+    phone = Column(String, unique=True)
+    email = Column(String, unique=True)
     parent_phone = Column(String)
     city = Column(String)
     grade = Column(String)
     lang = Column(String)
     password = Column(String)
-    student_code = Column(String, unique=True)
+    student_code = Column(String, unique=True, index=True, nullable=True)
